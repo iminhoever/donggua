ad319d0bf7cae188951dfbd16b4451fca5c6fbf8
LerongWang <iminhoever@163.com>
2017-08-23 20:49:20 +0800

    哈哈哈

---
+++ e3-manager/e3-manager-interface/src/main/java/com/rong/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.rong.service;
+
+import com.e3mall.pojo.TbItem;
+
+public interface ItemService {
+
+	public TbItem getItemById(long itemId);
+	
+}
---
+++ e3-manager/e3-manager-service/src/main/java/com/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,32 @@
+package com.e3mall.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.e3mall.mapper.TbItemMapper;
+import com.e3mall.pojo.TbItem;
+import com.e3mall.pojo.TbItemExample;
+import com.e3mall.pojo.TbItemExample.Criteria;
+import com.rong.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper tbItemMapper;
+
+	@Override
+	public TbItem getItemById(long itemId) {
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		if (list != null && list.size() > 0) {
+			return list.get(0);
+		}
+		return null;
+	}
+
+}
